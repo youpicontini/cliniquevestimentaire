f058a96efa145fd44db8d14b17a071926129996a
youpicontini <alexcontini@hotmail.com>
2017-03-21 10:15:56 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,53 @@
+
+$(document).ready(function(){
+
+    $('.collection').click(function() {
+        $('.collection').css('color','black');
+        $('.collection').css('background-color','white');
+    });
+
+    $('#switch-fr').click(function() {
+        $('.en').hide();
+        $('.fr').fadeIn(500);
+        $('#switch-fr').css('font-weight','bold')
+        $('#switch-en').css('font-weight','normal')
+    });
+
+    $('#switch-en').click(function() {
+        $('.fr').hide();
+        $('.en').fadeIn(500);
+        $('#switch-en').css('font-weight','bold')
+        $('#switch-fr').css('font-weight','normal')
+    });
+});
+
+
+$(window).scroll(function () {
+
+    $('.row').each(function(){
+        var pos = $(this).offset(),
+            wX = $(window).scrollLeft(), wY = $(window).scrollTop(),
+            wH = $(window).height(), wW = $(window).width(),
+            oH = $(this).outerHeight(), oW = $(this).outerWidth();
+
+        // check the edges
+        if (pos.left >= wX && pos.top >= wY &&
+            oW + pos.left <= wX + wW && oH + pos.top <= wY + wH )
+            {
+                $(this).children( '.first' ).fadeIn(1000);
+                $(this).children('.second').delay(1000).fadeIn(1000);
+            }
+        else
+        if (((pos.left <= wX && pos.left + oW > wX) ||
+            (pos.left >= wX && pos.left <= wX + wW)) &&
+            ((pos.top <= wY && pos.top + oH > wY)   ||
+                (pos.top  >= wY && pos.top  <= wY + wH)))
+            {
+                $(this).children( '.first' ).fadeIn(1000);
+                $(this).children('.second').delay(1000).fadeIn(1000);
+            }
+        /*else{
+            $(this).children().hide();
+        }*/
+    });
+});
